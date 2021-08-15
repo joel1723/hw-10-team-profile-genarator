f25f755c456283bab070f9d0dacdc52e24cff9e9
joel1723 <75437050+joel1723@users.noreply.github.com>
2021-08-14 22:48:07 -0400

    done

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const inquirer =require("inquirer")
 const path =require("path")
 const fs =require("fs")
 const managerArray=[]
+const internArray=[]
+const enginerArray=[]
+
+
+
 
 function createManager(){
     inquirer.prompt([
@@ -38,4 +43,74 @@ function createManager(){
         //call next funtion//
     })
 
+
+
+
+
+
+
+}
+function createIntern(){
+    inquirer.prompt([
+        {
+            type:"input",
+            name:"InternName",
+            message:"what is the intern name?",
+    
+        },
+        {
+            type:"input",
+            name:"internsId",
+            message:"what is the intern id?",
+    
+        },
+        {
+            type:"input",
+            name:"internEmail",
+            message:"what is the intern email?",
+    
+        },
+        {
+            type:"input",
+            name:"internSchool",
+            message:"what is the interns school?",
+    
+        },
+    ]).then(function(answers){
+        const Intern = new Intern (answers.internName,answers.internId,answers.internEmail,answers.internSchool)
+        internArray.push(intern)
+        //call next funtion//
+    })
+}
+function createEnginer(){
+    inquirer.prompt([
+        {
+            type:"input",
+            name:"enginerName",
+            message:"what is the enginer name?",
+    
+        },
+        {
+            type:"input",
+            name:"enginerId",
+            message:"what is the enginer id?",
+    
+        },
+        {
+            type:"input",
+            name:"enginerEmail",
+            message:"what is the enginer email?",
+    
+        },
+        {
+            type:"input",
+            name:"enginerGithub",
+            message:"what is the enginer github?",
+    
+        },
+    ]).then(function(answers){
+        const manager = new Manager (answers.managerName,answers.managerId,answers.managerEmail,answers.managerOffice)
+        managerArray.push(manager)
+        //call next funtion//
+    })
 }
